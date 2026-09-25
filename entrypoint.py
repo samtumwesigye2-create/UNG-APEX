ca7a144b21b741fa1b473eb6cd8e789dbe16d757
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-25 17:15:28 -0400

    Mount live UNG-SCM V4 event receiver

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,7 @@
+from scm_runtime import router as scm_runtime_router
 from app import app, shipments
 from nova_kpis import install_nova_kpi_routes
 
 install_nova_kpi_routes(app, shipments)
+
+app.include_router(scm_runtime_router)
